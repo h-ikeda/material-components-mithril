908df4874d26e81c661766a6ee684b414faffd2e
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-04-01 14:25:15 +0000

    Remove gratuitous mdcAttributes.

--- components/commons/vnode.js
+++ components/commons/vnode.js
@@ -1,10 +1,19 @@
 import m from "mithril";
 
+function objectFilter(obj, filter) {
+    Object.keys(obj).forEach((k, i) => {
+        if (!filter(k, i, obj)) {
+            delete obj[k];
+        }
+    });
+    return obj;
+}
+
 export default (vnode, tagName, classNames, classFilter) =>
-    m(vnode.attrs.tag || tagName, Object.assign({}, vnode.attrs, {
+    m(vnode.attrs.tag || tagName, objectFilter(Object.assign({}, vnode.attrs, {
         class: (vnode.attrs.class || "").split(" ").filter(c => c)
             .concat(Object.keys(Object.assign(classNames, vnode.attrs.classNames || {})).filter(classFilter || (() => 1)))
             .filter((v, i, s) => s.indexOf(v) === i)
-            .map(c => (this ? this[c]: "") || classNames[c], vnode.attrs.classNames)
+            .map(c => (vnode.attrs.classNames ? vnode.attrs.classNames[c]: "") || classNames[c])
             .join(" ")
-    }), vnode.children);
+    }), (k, _, s) => !(s.classNames ? s.classNames[k]: classNames[k])), vnode.children);
